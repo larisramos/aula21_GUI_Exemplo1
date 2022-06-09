203e7cbd1975617db54782c6059561d38067e9f8
Larissa Ramos <68797820+larisramos@users.noreply.github.com>
2022-06-09 11:39:32 +0000

    Outro exemplo adicionado

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+'''from tkinter import *
 
 class Application (Frame):
   def __init__ (self, master=None):
@@ -10,4 +10,26 @@ class Application (Frame):
     self.pack()
 
 app = Application()
+mainloop()'''
+#-------------Outro exemplo--------------
+from tkinter import *
+
+class Application (Frame):
+  
+  def texto(self):
+    self.msg.config(text= "ACHOU!!!")
+  
+  def __init__ (self, master=None):
+    Frame.__init__(self, master)
+    self.msg = Label(self)
+    self.msg.pack()
+    self.txt=Entry(self)
+    self.txt.pack()
+    self.bye = Button (self, text= "Bye", command=self.texto)
+    self.bye.pack()
+    self.pack()
+
+app = Application()
+app.master.title("Exemplo")
+app.master.geometry("200x200+200+200")
 mainloop()
